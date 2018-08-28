360c6607bb3d623ee39545e0ee9f0c980b87398f
nickreit@icloud.com <nickreit@icloud.com>
2018-08-28 19:37:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,8 @@ function logWhisper() {
 function sayHiToGrandma(string) {
   if (string === "hello")
   return ("I can't hear you!")
-  }
+  else if (string === "HELLO") 
+  return ("YES INDEED!")
+  else if (string === "I love you, Grandma.") 
+  return ("I love you, too.")
+      }
